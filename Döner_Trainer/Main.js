944ab521ff80f64d59e1b5475a5fa78d95560291
Verena <vero_ds@web.de>
2022-02-02 17:37:19 +0100

    Döner_Trainer_Update
    drawed Counter, Cuttingboard, Salad and Staff

--- Döner_Trainer/Main.js
+++ Döner_Trainer/Main.js
@@ -2,20 +2,14 @@
 var EIA2_Endabgabe_Döner_Trainer;
 (function (EIA2_Endabgabe_Döner_Trainer) {
     let formData;
-    // let staffAmount: number;
-    // let storageCapacity: number;
-    // let containerCapacity: number;
-    // let kebap: string[] = ["flatbread", "vegan meat"];
-    // let lahmacun: string[] = ["flatbread", "vegan mincemeat", "tomatosauce"];
-    // let yufka: string[] = ["thin flatbread", "vegan meat", "salad", "herb"];
     window.addEventListener("load", handleLoad);
-    function handleLoad() {
+    function handleLoad(_event) {
         let startButton = document.querySelector("#startButton");
         startButton.addEventListener("click", prepareGame);
         document.getElementById("gamefield").hidden = true;
-        //drawBackground();
+        document.getElementById("time").hidden = true;
     }
-    function prepareGame() {
+    function prepareGame(_event) {
         formData = new FormData(document.forms[0]);
         let form = document.querySelector("form");
         let body = document.querySelector("body");
@@ -25,14 +19,130 @@ var EIA2_Endabgabe_Döner_Trainer;
     }
     function buildGamescreen() {
         document.getElementById("gamefield").hidden = false;
+        document.getElementById("time").hidden = false;
         let canvas = document.querySelector("canvas");
         EIA2_Endabgabe_Döner_Trainer.crc2 = canvas.getContext("2d");
+        drawCounter(new EIA2_Endabgabe_Döner_Trainer.Vector(550, 370));
+        drawCuttingboard(new EIA2_Endabgabe_Döner_Trainer.Vector(800, 100));
+        drawContainer(new EIA2_Endabgabe_Döner_Trainer.Vector(1100, 370));
+        drawCustomer(new EIA2_Endabgabe_Döner_Trainer.Vector(200, 300));
+        drawSalad(new EIA2_Endabgabe_Döner_Trainer.Vector(800, 470));
+        drawTomatoes(new EIA2_Endabgabe_Döner_Trainer.Vector(900, 470));
     }
-    function drawBackground(_position) {
-        //counter
+    function drawCounter(_position) {
+        //draw counter
+        EIA2_Endabgabe_Döner_Trainer.crc2.beginPath();
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillStyle = "#8B4513";
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillRect(_position.x, _position.y, 800, 150);
+    }
+    function drawCuttingboard(_position) {
+        //draw cuttingboard
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillStyle = "#8B4513";
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillRect(_position.x, _position.y, 200, 100);
+        EIA2_Endabgabe_Döner_Trainer.crc2.save();
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillStyle = "#B9773A";
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillRect(_position.x + 25, _position.y + 9, 150, 80);
+    }
+    function drawContainer(_position) {
+        //draw container
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillStyle = "#696969";
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillRect(_position.x, _position.y, 65, 65);
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillRect(_position.x - 100, _position.y, 65, 65);
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillRect(_position.x - 200, _position.y, 65, 65);
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillRect(_position.x, _position.y + 85, 65, 65);
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillRect(_position.x - 100, _position.y + 85, 65, 65);
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillRect(_position.x - 200, _position.y + 85, 65, 65);
+    }
+    function drawCustomer(_position) {
+        //Body
+        EIA2_Endabgabe_Döner_Trainer.crc2.beginPath();
+        EIA2_Endabgabe_Döner_Trainer.crc2.translate(_position.x, _position.y);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(130, 19, 50, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillStyle = "lightblue";
+        EIA2_Endabgabe_Döner_Trainer.crc2.fill();
+        EIA2_Endabgabe_Döner_Trainer.crc2.closePath();
+        EIA2_Endabgabe_Döner_Trainer.crc2.beginPath();
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillStyle = "#FBF2EF";
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(132, -43, 30, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.fill();
+        EIA2_Endabgabe_Döner_Trainer.crc2.closePath();
+        //Hair
         EIA2_Endabgabe_Döner_Trainer.crc2.beginPath();
         EIA2_Endabgabe_Döner_Trainer.crc2.fillStyle = "brown";
-        EIA2_Endabgabe_Döner_Trainer.crc2.fillRect(_position.x + -300, _position.y + 400, 25, 110);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(132, -69, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(140, -69, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(154, -68, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(122, -69, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(132, -79, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(143, -79, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(122, -79, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(111, -75, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(114, -65, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.fill();
+        //Eyes
+        EIA2_Endabgabe_Döner_Trainer.crc2.beginPath();
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillStyle = "black";
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(125, -47, 2.5, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(138, -47, 2.5, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.closePath();
+        EIA2_Endabgabe_Döner_Trainer.crc2.fill();
+        //Mouth
+        EIA2_Endabgabe_Döner_Trainer.crc2.beginPath();
+        //zu 10,10 gehen
+        EIA2_Endabgabe_Döner_Trainer.crc2.moveTo(123, -30);
+        //bis 200,10 zeichnen
+        EIA2_Endabgabe_Döner_Trainer.crc2.lineTo(140, -30);
+        //Pfad schließen
+        EIA2_Endabgabe_Döner_Trainer.crc2.closePath();
+        //Linie Zeichnen
+        EIA2_Endabgabe_Döner_Trainer.crc2.stroke();
+        //Hands
+        EIA2_Endabgabe_Döner_Trainer.crc2.beginPath();
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillStyle = "#FBF2EF";
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(168, 15, 7, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(93, 15, 7, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.closePath();
+        EIA2_Endabgabe_Döner_Trainer.crc2.fill();
+        //Hands2
+        EIA2_Endabgabe_Döner_Trainer.crc2.beginPath();
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillStyle = "grey";
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(150, 60, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(110, 60, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.closePath();
+        EIA2_Endabgabe_Döner_Trainer.crc2.fill();
+        EIA2_Endabgabe_Döner_Trainer.crc2.restore();
+        EIA2_Endabgabe_Döner_Trainer.crc2.closePath();
+        EIA2_Endabgabe_Döner_Trainer.crc2.beginPath();
+    }
+    function drawSalad(_position) {
+        EIA2_Endabgabe_Döner_Trainer.crc2.translate(_position.x, _position.y);
+        EIA2_Endabgabe_Döner_Trainer.crc2.beginPath();
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillStyle = "green";
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(132, -69, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(140, -69, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(154, -68, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(122, -69, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(132, -79, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(143, -79, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(122, -79, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(111, -75, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(114, -65, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.fill();
+    }
+    function drawTomatoes(_position) {
+        EIA2_Endabgabe_Döner_Trainer.crc2.translate(_position.x, _position.y);
+        EIA2_Endabgabe_Döner_Trainer.crc2.beginPath();
+        EIA2_Endabgabe_Döner_Trainer.crc2.fillStyle = "red";
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(132, -69, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(140, -69, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(154, -68, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(122, -69, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(132, -79, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(143, -79, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(122, -79, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(111, -75, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.arc(114, -65, 10, 0, 2 * Math.PI);
+        EIA2_Endabgabe_Döner_Trainer.crc2.fill();
     }
 })(EIA2_Endabgabe_Döner_Trainer || (EIA2_Endabgabe_Döner_Trainer = {}));
 //# sourceMappingURL=Main.js.map
--- Döner_Trainer/Main.ts
+++ Döner_Trainer/Main.ts
@@ -1,6 +1,10 @@
 namespace EIA2_Endabgabe_Döner_Trainer {
 
     let formData: FormData;
+    //let gameStart: boolean = false;
+    //let gameTimer: number;
+    //let countDown: number;
+    //let time: HTMLDivElement;
     // let staffAmount: number;
     // let storageCapacity: number;
     // let containerCapacity: number;
@@ -9,23 +13,22 @@ namespace EIA2_Endabgabe_Döner_Trainer {
     // let yufka: string[] = ["thin flatbread", "vegan meat", "salad", "herb"];
 
 
-    window.addEventListener("load", handleLoad);
-
     export let crc2: CanvasRenderingContext2D;
     export let canvas: HTMLCanvasElement | null;
 
-    function handleLoad(): void {
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad(_event: Event): void {
 
         let startButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#startButton")!;
         startButton.addEventListener("click", prepareGame);
 
         document.getElementById("gamefield").hidden = true;
-
-        //drawBackground();
+        document.getElementById("time").hidden = true;
 
     }
 
-    function prepareGame(): void {
+    function prepareGame(_event: Event): void {
 
         formData = new FormData(document.forms[0]);
 
@@ -35,19 +38,152 @@ namespace EIA2_Endabgabe_Döner_Trainer {
         body.removeChild(form);
         buildGamescreen();
         console.log(formData);
+
     }
 
     function buildGamescreen(): void {
+
         document.getElementById("gamefield").hidden = false;
+        document.getElementById("time").hidden = false;
+
+
+
         let canvas: HTMLCanvasElement = document.querySelector("canvas")!;
         crc2 = canvas.getContext("2d")!;
+
+        drawCounter(new Vector(550, 370));
+        drawCuttingboard(new Vector(800, 100));
+        drawContainer(new Vector(1100, 370));
+        drawCustomer(new Vector(200, 300));
+        drawSalad(new Vector(800, 470));
+        drawTomatoes(new Vector(900, 470));
+
+
     }
 
-    function drawBackground(_position: Vector): void {
-        //counter
+    function drawCounter(_position: Vector): void {
+        //draw counter
+        crc2.beginPath();
+        crc2.fillStyle = "#8B4513";
+        crc2.fillRect(_position.x, _position.y, 800, 150);
+    }
+
+    function drawCuttingboard(_position: Vector): void {
+        //draw cuttingboard
+        crc2.fillStyle = "#8B4513";
+        crc2.fillRect(_position.x, _position.y, 200, 100);
+        crc2.save();
+        crc2.fillStyle = "#B9773A";
+        crc2.fillRect(_position.x + 25, _position.y + 9, 150, 80);
+    }
+
+    function drawContainer(_position: Vector): void {
+        //draw container
+        crc2.fillStyle = "#696969";
+        crc2.fillRect(_position.x, _position.y, 65, 65);
+        crc2.fillRect(_position.x - 100, _position.y, 65, 65);
+        crc2.fillRect(_position.x - 200, _position.y, 65, 65);
+        crc2.fillRect(_position.x, _position.y + 85, 65, 65);
+        crc2.fillRect(_position.x - 100, _position.y + 85, 65, 65);
+        crc2.fillRect(_position.x - 200, _position.y + 85, 65, 65);
+
+    }
+    function drawCustomer(_position: Vector): void {
+        //Body
+        crc2.beginPath();
+        crc2.translate(_position.x, _position.y);
+        crc2.arc(130, 19, 50, 0, 2 * Math.PI);
+        crc2.fillStyle = "lightblue";
+        crc2.fill();
+        crc2.closePath();
+        crc2.beginPath();
+        crc2.fillStyle = "#FBF2EF";
+        crc2.arc(132, -43, 30, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.closePath();
+        //Hair
         crc2.beginPath();
         crc2.fillStyle = "brown";
-        crc2.fillRect(_position.x + -300, _position.y + 400, 25, 110);
+        crc2.arc(132, -69, 10, 0, 2 * Math.PI);
+        crc2.arc(140, -69, 10, 0, 2 * Math.PI);
+        crc2.arc(154, -68, 10, 0, 2 * Math.PI);
+        crc2.arc(122, -69, 10, 0, 2 * Math.PI);
+        crc2.arc(132, -79, 10, 0, 2 * Math.PI);
+        crc2.arc(143, -79, 10, 0, 2 * Math.PI);
+        crc2.arc(122, -79, 10, 0, 2 * Math.PI);
+        crc2.arc(111, -75, 10, 0, 2 * Math.PI);
+        crc2.arc(114, -65, 10, 0, 2 * Math.PI);
+        crc2.fill();
+        //Eyes
+        crc2.beginPath();
+        crc2.fillStyle = "black";
+        crc2.arc(125, -47, 2.5, 0, 2 * Math.PI);
+        crc2.arc(138, -47, 2.5, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        //Mouth
+        crc2.beginPath();
+        //zu 10,10 gehen
+        crc2.moveTo(123, -30);
+        //bis 200,10 zeichnen
+        crc2.lineTo(140, -30);
+        //Pfad schließen
+        crc2.closePath();
+        //Linie Zeichnen
+        crc2.stroke();
+        //Hands
+        crc2.beginPath();
+        crc2.fillStyle = "#FBF2EF";
+        crc2.arc(168, 15, 7, 0, 2 * Math.PI);
+        crc2.arc(93, 15, 7, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        //Hands2
+        crc2.beginPath();
+        crc2.fillStyle = "grey";
+        crc2.arc(150, 60, 10, 0, 2 * Math.PI);
+        crc2.arc(110, 60, 10, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.restore();
+        crc2.closePath();
+        crc2.beginPath();
+
+
     }
+
+    function drawSalad(_position: Vector): void {
+        crc2.translate(_position.x, _position.y);
+        crc2.beginPath();
+        crc2.fillStyle = "green";
+        crc2.arc(132, -69, 10, 0, 2 * Math.PI);
+        crc2.arc(140, -69, 10, 0, 2 * Math.PI);
+        crc2.arc(154, -68, 10, 0, 2 * Math.PI);
+        crc2.arc(122, -69, 10, 0, 2 * Math.PI);
+        crc2.arc(132, -79, 10, 0, 2 * Math.PI);
+        crc2.arc(143, -79, 10, 0, 2 * Math.PI);
+        crc2.arc(122, -79, 10, 0, 2 * Math.PI);
+        crc2.arc(111, -75, 10, 0, 2 * Math.PI);
+        crc2.arc(114, -65, 10, 0, 2 * Math.PI);
+        crc2.fill();
+    }
+    function drawTomatoes(_position: Vector): void {
+        crc2.translate(_position.x, _position.y);
+        crc2.beginPath();
+        crc2.fillStyle = "red";
+        crc2.arc(132, -69, 10, 0, 2 * Math.PI);
+        crc2.arc(140, -69, 10, 0, 2 * Math.PI);
+        crc2.arc(154, -68, 10, 0, 2 * Math.PI);
+        crc2.arc(122, -69, 10, 0, 2 * Math.PI);
+        crc2.arc(132, -79, 10, 0, 2 * Math.PI);
+        crc2.arc(143, -79, 10, 0, 2 * Math.PI);
+        crc2.arc(122, -79, 10, 0, 2 * Math.PI);
+        crc2.arc(111, -75, 10, 0, 2 * Math.PI);
+        crc2.arc(114, -65, 10, 0, 2 * Math.PI);
+        crc2.fill();
+    }
+
+
+
 
 }
